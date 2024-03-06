0336f563ca0377e511a5bfc4459c3bafd81c6aa0
Maximiliam Rojas <maximiliamrojas@gmail.com>
2024-03-06 16:13:31 -0300

    Controllers & Views

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.destroy_all
+@dorita = Restaurant.create!( name: "La Dorita", address: "Pete Docter", phone_number: "0981 222 610", category: "chinese" )
+@larosa = Restaurant.create!( name: "La Rosa de Guadalupe", address: "Palermo", phone_number: "0981 222 610", category: "italian" )
+@starbucks = Restaurant.create!( name: "Starbucks", address: "Recoleta", phone_number: "0981 222 610", category: "japanese" )
+@burgerking = Restaurant.create!( name: "Burger King", address: "Palermo", phone_number: "0981 222 610", category: "french" )
+@mc = Restaurant.create!( name: "McDonalds", address: "Recoleta", phone_number: "0981 222 610", category: "chinese" )
+p "Created #{Restaurant.count} restaurants"
+
+Review.destroy_all
+@dorita.reviews.create!(content: "Bien!", rating: 5)
+@larosa.reviews.create!(content: "mispoppersss", rating: 4)
+@starbucks.reviews.create!(content: "Nice!", rating: 3)
+@burgerking.reviews.create!(content: "Meh", rating: 2)
+@mc.reviews.create!(content: "Muy bueno", rating: 1)
+p "Created #{Review.count} reviews"
